e0cbf04d42e17c102a9a818819f46bb5c9aad4c4
manotasCoder <ruben.manotas.lopez@gmail.com>
2021-06-16 11:48:17 +0200

    entrando en juego

--- django/djangoFirst/hello/urls.py
+++ django/djangoFirst/hello/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('',views.index, name='index'),
     path('coder/',views.coder, name='greeting'),
+    path('<str:name>',views.greet, name='greet'),
+    path('<str:name>/',views.greet, name='greet'),
 ]
